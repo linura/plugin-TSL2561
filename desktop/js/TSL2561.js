9e7881c7699fabfc1f5ba790609f8322cf7f7441
LEBANSAIS <c.lebansais@icloud.com>
2020-05-19 19:46:33 +0200

    Update TSL2561.js

--- desktop/js/TSL2561.js
+++ desktop/js/TSL2561.js
@@ -18,7 +18,7 @@
 
 $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
 /*
- * Fonction pour l'ajout de commande, appellé automatiquement par plugin.template
+ * Fonction pour l'ajout de commande, appellé automatiquement par plugin.TSL2561
  */
 function addCmdToTable(_cmd) {
     if (!isset(_cmd)) {
