c8a83a8c47753793f507f5c5f0c5f4dee667ff4d
Linura <13700448+linura@users.noreply.github.com>
2020-05-20 13:47:02 +0200

    Update TSL2561.php

--- desktop/php/TSL2561.php
+++ desktop/php/TSL2561.php
@@ -98,6 +98,7 @@ foreach (jeeObject::all() as $object) {
         <label class="col-sm-3 control-label">{{0=1x, 1=16x}}</label>
         <div class="col-sm-3">
             <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="Gain" placeholder="1"/>
+        </div>
         <label class="col-sm-3 control-label">{{Temps d'integration}}</label>
         <label class="col-sm-3 control-label">{{(0=13.7ms, 1=101ms, 2=402ms, or 3=manual)</label>
         <div class="col-sm-3">
